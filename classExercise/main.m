8ac2c668068b39ee8ac6142ff04e5037cbcad2bd
zjj <jj.weber@gmail.com>
2012-08-08 13:29:54 -0700

    Initial Commit

---
+++ classExercise/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  classExercise
+//
+//  Created by JJ Weber on 8/8/12.
+//  Copyright (c) 2012 JJ Weber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
